a71275fa19781fc24f737e2bc9f9ae4e078e452f
Camille PRADO <camille.prado@felisse.fr>
2025-10-22 16:58:25 +0200

    Modification de db.js afin d'interagir avec une base de données avec un nom précis, au lieu de 'test', comme on faisait avant.

--- db.js
+++ db.js
@@ -5,7 +5,12 @@ dotenv.config();
 
 let connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(
+      process.env.MONGODB_URI,
+      {
+        dbName:process.env.DB_NAME || 'FoodExpress'
+      }
+    );
     console.log(`Connecté à MongoDB !`)
   } catch(error) {
     console.error(`/!\\ ERREUR - MONGODB /!\\\n${error}`)

003f5aca29e1ea4e22b067416edca4421ce5c835
adysel <sedayangir.pro@gmail.com>
2025-10-25 14:20:16 +0200

    Add files via upload

--- routes/menusRoutes.js
+++ routes/menusRoutes.js
@@ -10,39 +10,82 @@ const { requireAuth, requireRole } = require("../middleware/auth");
 const router = express.Router();
 router.use(express.json());
 
+
+// swagger voir menus 
+/**
+ * @swagger
+ * /users/<ID>:
+ *  get:
+ *    summary: Accès aux données de menu
+ *    description: Permet la lecture de menu
+ *    security:
+ *      - bearerAuth: []
+ *    parameters: 
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: ID du menu à afficher
+ *        schema:
+ *          type: string
+ *          example: 68f8f09b07afe91a0a2b6e5d
+ * 
+ *    responses:
+ *      200:
+ *        description: Données menus affichées
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Menu'
+ *      404:
+ *        description: Menu non trouvé
+ *      
+ */
 // voir les Menus
 router.get("/",async (req,res) => {
-  try {
-    // Extraction des paramètres de requête avec valeurs par défaut
-    const { search = "", sortBy = "name", order = "asc", limit = 10, page = 1 } = req.query;
-
-    // Création de l'expression régulière pour la recherche
-    const searchRegex = new RegExp(search, "i");
-    const filter = {
-      $or: [
-        { name: { $regex: searchRegex } },
-        { address: { $regex: searchRegex } }
-      ]
-    };
-
-    // Détermination de l’ordre de tri (ascendant ou descendant)
-    const sortOrder = order === "desc" ? -1 : 1;
-
-    // Pagination
-    const skip = (parseInt(page) - 1) * parseInt(limit);
-
-    // Requête avec filtre, tri et pagination sur les menus
-    const _Menu = await Menus.find(filter)
-      .sort({ [sortBy]: sortOrder })
-      .skip(skip)
-      .limit(parseInt(limit));
-  return res.status(200).json(_Menu)
-  } catch {
+  if (process.env.NODE_ENV === "development") {
+    const menus = await Menus.find()
+    return res.status(418).json(menus)
+  } else {
     return res.status(300).json({error:"Not allowed !"})
   }
 })
 
-
+// swagger création de menus
+/**
+ * @swagger
+ * /menus:
+ *  post:
+ *   summary: Création de menu
+ *   description: Création de menu
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/MenusCreate'
+ *      
+ *   responses:
+ *     201:
+ *       description: Menu créé
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items: 
+ *               $ref: '#/components/schemas/Menus'
+ *     400:
+ *       description: Erreur de validation
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ *     409:
+ *       description: Erreur car duplicata de menu
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ */
 // Création de Menus
 router.post("/",  requireAuth, async (req, res) => {
   // Validation des données envoyées avant création
@@ -56,12 +99,82 @@ router.post("/",  requireAuth, async (req, res) => {
   res.status(201).json(menu);
 });
 
+
+// swagger lecture menus 
+/** 
+ @swagger
+ * /menus:
+ *  get:
+ *    summary: Gestion des menus
+ *    description: Surtout pour débugger, renvoie un array avec les menus et leur IDs, name, description, prix et catégorie
+ *    responses:
+ *      200:
+ *        description: Array de/menus
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Menus'
+ *      403:
+ *        description: Tentative d'accès au noeud en production
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
+
 // Lecture  menus
-router.get("/:id", async (req, res) => {
+router.get("/:id", requireAuth, async (req, res) => {
   const _Menus = await Menus.findById(req.params.id);
   if (!_Menus) return res.status(404).json({ message: "user not found" });
   res.json(_Menus);
 });
+
+
+// swagger patch menu
+/**
+ * @swagger
+ * /menus/<ID>:
+ *  patch:
+ *    description: Modification de menu
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: ID du menu à modifier
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Menus'
+ *    responses:
+ *      400:
+ *        description: Erreur de validation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      404:
+ *        description: Menu non trouvé
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ * 
+ *      200:
+ *       description: Données Menu modifiées
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items: 
+ *               $ref: '#/components/schemas/Menus'
+ *              
+ */
 
 // MAJ de menus, validation des données avec JOI
 router.patch("/:id", requireAuth, async (req, res) => {
@@ -76,6 +189,34 @@ router.patch("/:id", requireAuth, async (req, res) => {
   res.json(_Menus);
 });
 
+// swagger menus suppression
+/**
+ *  @swagger
+ *  /menus/<ID>:
+ *    delete:
+ *      description: Suppression de menus
+ *      security: 
+ *        - bearerAuth: []
+ *      parameters: 
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          description: L'ID du menu à supprimer
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: Menus supprimé avec succès
+ *  
+ *        400:
+ *          description: Erreur interne lors de la suppresssion
+ *          content:
+ *            application/json:
+ *              schema:
+ *                items:
+ *                  $ref: '#/components/schemas/Error'
+ *            
+ */
 // Suppression  Menus
 router.delete("/:id", requireAuth, async (req, res) => {
   try {
@@ -86,6 +227,4 @@ router.delete("/:id", requireAuth, async (req, res) => {
     return res.status(400).json({ message: "Invalid ID format" });
   }
 });
-
-
 module.exports = router
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -10,6 +10,35 @@ const { requireAuth, requireRole } = require("../middleware/auth");
 const router = express.Router();
 router.use(express.json());
 
+// swagger voir restaurant 
+/**
+ * @swagger
+ * /restaurants/<ID>:
+ *  get:
+ *    summary: Accès aux données restaurant
+ *    description: Permet la lecture resataurant
+ *    security:
+ *      - bearerAuth: []
+ *    parameters: 
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: ID du restaurant à afficher
+ *        schema:
+ *          type: string
+ *          example: 68f8f09b07afe91a0a2b6e5d
+ * 
+ *    responses:
+ *      200:
+ *        description: Données restaurant affichées
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Restaurants'
+ *      404:
+ *        description: Restaurant non trouvé
+ *      
+ */
 // voir tous les restaurants
 router.get("/",async (req,res) => {
   try {
@@ -42,7 +71,42 @@ router.get("/",async (req,res) => {
   }
 })
 
-
+// swagger création de restaurant
+/**
+ * @swagger
+ * /restaurants:
+ *  post:
+ *   summary: Création de restaurant
+ *   description: Création de restaurant
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: '#/components/schemas/RestaurantsCreate'
+ *      
+ *   responses:
+ *     201:
+ *       description: Restaurant créé
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items: 
+ *               $ref: '#/components/schemas/Restaurants'
+ *     400:
+ *       description: Erreur de validation
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ *     409:
+ *       description: Erreur car duplicata du restaurant
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Error'
+ */
 // Création de restaurant 
 router.post("/",  requireAuth, async (req, res) => {
   // Validation des données 
@@ -60,12 +124,79 @@ router.post("/",  requireAuth, async (req, res) => {
   res.status(201).json(newRestaurants);
 });
 
+// swagger lecture restaurants 
+/** 
+ @swagger
+ * /restaurants:
+ *  get:
+ *    summary: Gestion des restaurant
+ *    description: Pour débugger, renvoie un array avec les restaurants et leur IDs, name, adresse, phone et heure d'ouverture
+ *    responses:
+ *      200:
+ *        description: Array de/restaurants
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Restaurants'
+ *      403:
+ *        description: Tentative d'accès au noeud en production
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 // Lecture restaurant 
-router.get("/:id", async (req, res) => {
+router.get("/:id", requireAuth, async (req, res) => {
   const _Restaurant = await Restaurants.findById(req.params.id);
   if (!_Restaurant) return res.status(404).json({ message: "Restaurant not found" });
   res.json(_Restaurant);
 });
+
+// swagger patch restaurant
+/**
+ * @swagger
+ * /restaurants/<ID>:
+ *  patch:
+ *    description: Modification de restaurant
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: ID du restaurant à modifier
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Restaurants'
+ *    responses:
+ *      400:
+ *        description: Erreur de validation
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      404:
+ *        description: Restaurant non trouvé
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ * 
+ *      200:
+ *       description: Données Restaurant modifiées
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items: 
+ *               $ref: '#/components/schemas/Restaurants'
+ *              
+ */
 
 // MAJ des données restaurants, validation des données avec JOI
 router.patch("/:id", requireAuth, async (req, res) => {
@@ -77,14 +208,42 @@ router.patch("/:id", requireAuth, async (req, res) => {
   const _Restaurant = await Restaurants.findByIdAndUpdate(req.params.id, value, { new: true });
   if (!_Restaurant) return res.status(404).json({ message: "Restaurant not found" });
 
-  return res.status(200).json(_Restaurant);
+  res.json(Restaurants);
 });
 
+// swagger restaurants suppression
+/**
+ *  @swagger
+ *  /restaurants/<ID>:
+ *    delete:
+ *      description: Suppression de restaurants
+ *      security: 
+ *        - bearerAuth: []
+ *      parameters: 
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          description: L'ID du restaurant à supprimer
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: Restaurants supprimé avec succès
+ *  
+ *        400:
+ *          description: Erreur interne lors de la suppresssion
+ *          content:
+ *            application/json:
+ *              schema:
+ *                items:
+ *                  $ref: '#/components/schemas/Error'
+ *            
+ */
 // Suppression resaturant par ID 
 router.delete("/:id", requireAuth, async (req, res) => {
   try {
     const _Restaurant = await Restaurants.findByIdAndDelete(req.params.id);
-    if (!_Restaurant) return res.status(404).json({ message: "Restaurant not found" });
+    if (!_Restaurant) return res.status(404).json({ message: "User not found" });
     return res.json({ message: `User ${_Restaurant.name} deleted successfully` });  
   } catch(error) {
     return res.status(400).json({ message: "Invalid ID format" });
